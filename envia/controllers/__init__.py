3c67e26024de2ee268b844e24d36af0712cc8de1
malcomprado-sudo <malcom.prado@envia.com>
2026-09-01 10:51:11 -0600

    Delete envia directory

--- envia/controllers/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import envia_integration
-from . import website_sale_envia
--- envia/static/src/js/envia_wizard_noop_action.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module **/
-
-import { registry } from "@web/core/registry";
-
-// ponytail: Odoo 19 maps falsy call_button results to act_window_close; modal wizards use this instead.
-registry.category("actions").add("envia_wizard_noop", () => {});
